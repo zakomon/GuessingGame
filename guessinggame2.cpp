8e950cd89f0263a4fa35baaa891676df867802b8
Rahul Narayanan <rahulnari10@yahoo.com>
2016-01-13 16:20:05 +0530

    commit

--- guessinggame2.cpp
+++ guessinggame2.cpp
@@ -23,11 +23,13 @@ int binary_search(int guess,int high,int low,int count){
 
 
 int main(){
-	srand(time(NULL));
+	for(int i = 0; i <= 100; i++){
+		srand(time(NULL));
 
- 	int gues = (rand() % 1024) + 1;
- 	int x = binary_search(gues,1024,1,0);
- 	cout << gues << endl;
+	 	int gues = (rand() % 1024) + 1;
+	 	int x = binary_search(gues,1024,1,0);
+	 	cout << gues << endl;
 
-	cout << x << endl;
+		cout << x << endl;
+	}
 }
