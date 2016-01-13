5ead52ddca8cb15f24bbe756972c24c6b9e408e1
Rahul Narayanan <rahulnari10@yahoo.com>
2016-01-13 16:00:04 +0530

    updated guessinggame2.cpp

--- guessinggame2.cpp
+++ guessinggame2.cpp
@@ -7,20 +7,18 @@ using namespace std;
 int binary_search(int guess,int high,int low,int count){
 	int cguess = (high + low) / 2;
 	count++;
-	for(int i = 0; i <= 100; i++){
-		if(cguess != guess){
+	if(cguess != guess){
 
-			if(cguess > guess){
-				return binary_search(guess,cguess,low,count);
-			}
-			else if(cguess < guess){
-				return binary_search(guess,high,cguess,count);
-			}
+		if(cguess > guess){
+			return binary_search(guess,cguess - 1,low,count);
+		}
+		else if(cguess < guess){
+			return binary_search(guess,high,cguess + 1,count);
+		}
 			
 
-		}
-		return count;
 	}
+	return count;
 }
 
 
@@ -30,5 +28,6 @@ int main(){
  	int gues = (rand() % 1024) + 1;
  	int x = binary_search(gues,1024,1,0);
  	cout << gues << endl;
+
 	cout << x << endl;
 }
